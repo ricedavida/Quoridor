21521b0ac8dd9aa444c6541f8f5fd5dd5661c9d8
David Rice <rice.davida@gmail.com>
2014-02-03 18:59:36 -0500

    simple derp JUnit thing

---
+++ Quoridor/src/ThingTester.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class ThingTester extends TestCase {
+	public ThingTester (String name) {
+		super (name);
+	}
+	
+	public static void main(String[] args) {
+		TestRunner.runAndWait(new TestSuite(ThingTester.class));
+	}
+
+	@Test
+	public void testGetName() throws Exception {
+	    String fileSpec = new String("c:xxxyyyzzz.txt");
+	    assertEquals("zzz.txt", getName(fileSpec));
+	}
+	
+	private String getName(String fileSpec) {
+		return fileSpec;
+	}
+}
