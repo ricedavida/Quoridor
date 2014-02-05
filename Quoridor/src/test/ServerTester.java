bbaee59532a53fca7ca80cd864af19e0b694aaf9
David Rice <rice.davida@gmail.com>
2014-02-05 18:16:25 -0500

    Created a ServerTester.java file.  This file will test server functionality.  At this moment the only test it has fails.

---
+++ Quoridor/src/test/ServerTester.java
@@ -0,0 +1,39 @@
+/*
+ * ServerTester.java
+ * Authors: David Rice
+ * This class will test Server.java functionality so
+ * things don't break.
+ */
+
+package test;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class ServerTester extends TestCase{
+
+	public ServerTester (String name) { 
+		super(name);
+	}
+
+	// This test will see if the message "hi" is being recieved
+	@Test
+	public void testGetServerMessage() throws Exception {
+		String str = new String("hi");
+		assertEquals("", getServerMessage(str));
+	}
+	
+	private String getServerMessage(String str) {
+		return str;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestRunner.runAndWait(new TestSuite(ServerTester.class));
+	}
+
+}
