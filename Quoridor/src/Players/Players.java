7014cf80c1c9ee5daffb747eceb473d4ab106a6f
pryoreb197 <pryoreb197@potsdam.edu>
2014-03-13 15:00:40 -0400

    Added some functionality to players. Noticing some issues that will make using seperate player objects dificult.

--- Quoridor/src/Players/Players.java
+++ Quoridor/src/Players/Players.java
@@ -1,21 +1,23 @@
 package Players;
 //this class is intended to represent the palyers
+//not gonna lie, i do not see the use of decoupling the players from the board
 
 public class Players {
 
-		int playerName;
-		int numWalls;
-		boolean isKicked;
+		private int playerName;
+		private int totalPlayers;
+		private int numWalls;
+		private boolean isKicked;
 		// location is a single int now. 
 		// board is numbers from 0 to 80. 
-		int location;
+		private int location;
 		// players want to get to end
 		// this is a list of winning spaces
-		int[] end; 
+		private int[] end; 
 		// if a player wins, this becomes true
 		//	probably don't need, but whatever
-		boolean hasWon;
-		int [] possibleMoves;
+		private boolean hasWon;
+		private int [] possibleMoves;
 		// format of possible moves: 
 		// array of ints
 		// pM[0] = North
@@ -24,10 +26,12 @@ public class Players {
 		// pM[3] = east
 		
 		public Players(int playerId, int numPlayers){
+			// player initial possible moves are hard coded in, i think i can make this not so later.
 			playerName = playerId;
 			numWalls = 20 / numPlayers;
 			isKicked = false;
 			hasWon = false;
+			totalPlayers = numPlayers;
 			if(playerId == 1){
 				this.location = 4;
 				this.end = new int[8];
@@ -91,6 +95,30 @@ public class Players {
 			}
 		}
 		// methods go here
+		// getPos()
+		// args: null
+		// returns: int location
+		// numeric value representing position on the board
+		// the player is there right now.
+		public int getPos(){
+			return location;
+		}
 		
+		// getWallCount()
+		// args: null
+		// returns: int numWalls
+		// numeric value of number of walls this player has.
+		public int getWallCount(){
+			return numWalls;
+		}
 		
+		// helper method setPossible()
+		// args: null
+		// returns: null
+		// use this to calculate the current possible moves for a pawn
+		// call it after a move, from the move method.
+		// can not check for walls
+		private void setPossible(){
+			
+		}
 }
