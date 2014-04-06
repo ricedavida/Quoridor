bb84255a54288b0239813599932e3da87a67a5a8
Sean <kellehsr196@potsdam.edu>
2014-04-06 16:44:26 -0400

    Edited AI: FastestPathLeft/Right and positioning strategies

--- Quoridor/src/AI/AI_Methods.java
+++ Quoridor/src/AI/AI_Methods.java
@@ -1,10 +1,5 @@
-
 package AI;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 
 public class AI_Methods {
 	//String Array to represent board
@@ -20,72 +15,47 @@ public class AI_Methods {
 		{" "," "," "," ","2"," "," "," "," "},
 	};
 	static int pPositionC, pPositionL;
-	int Player1,Player2,Player3,Player4;
-	static int lastMove;
 	//list of all possible moves
-	static String list,fastestPathRight,fastestPathLeft="";
+	static String list,fastestPathLeft,fastestPathRight;
+	static String failedPaths[] = new String[100];
 	
-	public static String allPossibleMoves(String pawn){
+	public static void allPossibleMoves(String a){
 		list = "";
 		for(int i=0; i<81; i++)
-			if (board[i/9][i%9].equals(pawn)){
+			if (board[i/9][i%9].equals(a)){
 			    list = "";
 				list+=possiblePawnMoves(i);
+				System.out.println("Pawn "+a+"'s possible moves:"+list);
 			}
-		return list;
 	}
 	
-	public static int currentPositionOfPawn(String pawn){
-		int pos = -1;
+	/*public String currentPositionOfPawn(String pawn){
 		for(int i=0; i<81; i++)
 			if (board[i/9][i%9].equals(pawn)){
-				pos = i;
-		}
-		return pos;
-	}
-
-	public static int[] possiblePawnMovesInt(int i){
-		int[] moves = new int[5];
-		moves[0] = i;
-		moves[1] = -1;
-		moves[2] = -1;
-		moves[3] = -1;
-		moves[4] = -1;
-		
-		int r = i/9, c = i%9;
-		for(int j = 0; j<9; j++){
-			if (j == 1||j == 3||j == 5||j == 7){
-				try{
-					if(" ".equals(board[r-1+j/3][c-1+j%3])){
-						if(viableMove()){
-							if(j == 1 && i-9>0)
-								moves[1] = i-9;
-							if(j == 3 && i-1>0)
-								moves[3] = i-1;
-							if(j == 5 && i+1<81)
-								moves[4] = i+1;
-							if(j == 7 && i+9<81)
-								moves[2] = i+9;
-						}
-					}
-				}catch (Exception e) {}
+				int pos = i;
 			}
-		}
-		return moves;
-	}
+	
+	}*/
+
 	public static String possiblePawnMoves(int i){
 		list="";
-		String oldPiece;
+		 String oldPiece;
 		int r = i/9, c = i%9;
 		for(int j = 0; j<9; j++){
 			if (j == 1||j == 3||j == 5||j == 7){
 				try{
 					if(" ".equals(board[r-1+j/3][c-1+j%3])){
 						oldPiece = board[r-1+j/3][c-1+j%3];
+						board[r][c]=" ";
+						//board[r-1+j/3][c-1+j%3]="P";
+						int pTemp = pPositionC;
+						pPositionC=i+(j/3)*9+j%3-9;
 						if(viableMove()){
 							list = list+r+c+(r-1+j/3)+(c-1+j%3)+oldPiece;
 						}
+						//board[r][c]="P";
 						board[r-1+j/3][c-1+j%3]=oldPiece;
+						pPositionC = pTemp;
 					}
 				}catch (Exception e) {}
 			}
@@ -94,34 +64,20 @@ public class AI_Methods {
 		return list;
 	}
 	
-	public static String fastestPathLeft(int position){
-		int moves[] = possiblePawnMovesInt(position);
-		if(position == 3){}//Should be if lastMove == winCondition
-		
-		if(moves[1]!=-1){//moving up
-				fastestPathLeft += position+"^"+moves[1]+" ";
-				//System.out.println("LM = "+moves[1]);
-				fastestPathLeft(moves[1]);
-				return fastestPathLeft;
-		} 
-		if(moves[3]!=-1){//moving left
-			fastestPathLeft += position+"<"+moves[3]+" ";
-			fastestPathLeft(moves[3]);
-			return fastestPathLeft;
-		}
-		/*if(moves[4]!=-1){//moving left
-			fastestPathLeft += position+"<"+moves[4]+" ";
-			fastestPathLeft(moves[4]);
-			return fastestPathLeft;
-		}*/
-		return "Done";
+	public static String fastestPathLeft(String pos){
+		return fastestPathLeft;
 	}
+	
+	public static String fastestPathRight(String a[][]){
+		return fastestPathRight;
+	}
+	
 	public static boolean viableMove(){
 		return true;
 	}
 
 	public static void main(String args[]){
-		String a = "Shit";
+		String a = null;
 		System.out.println("Enter a pawn to find moves for:");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
@@ -129,12 +85,10 @@ public class AI_Methods {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-			/*System.out.println(allPossibleMoves(a));
-			System.out.println(currentPositionOfPawn(a));
-			int b = currentPositionOfPawn(a);
-			int moves[] = possiblePawnMovesInt(b);
-			for(int i=0;i<moves.length;i++)
-				System.out.println(moves[i]);*/
-			System.out.println(fastestPathLeft(Integer.parseInt(a)));
+		if(a.equals("1")||a.equals("2")||a.equals("3")||a.equals("4"))
+			allPossibleMoves(a);
+		else
+			System.out.println("Not valid input");
+		
 	}
 }
